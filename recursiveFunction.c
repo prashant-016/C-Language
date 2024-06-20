6b7dd556e11d49f696b05e73ef0f9239cf8b30a0
Prashant Ojha <prashantojha288@gmail.com>
2024-06-20 14:13:27 +0530

    find factorial number using recursion

---
+++ recursiveFunction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+long int multiply numbers (int n);
+int main(){
+    int n;
+    printf("Enter a positive integer:");
+    scanf("Factorial of %d=1%d",n,multiply number(n));
+    return 0;
+}
+long int multiply number(int n){
+    if(n>=1)
+    return n*multiply number (n-1);
+    else
+    return 1;
+}
